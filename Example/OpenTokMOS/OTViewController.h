c50a150834a274be7d5e35332fe656984ba9ca7a
Charley Robinson <charley@tokbox.com>
2016-12-05 22:07:47 +0100

    Initial commit

---
+++ Example/OpenTokMOS/OTViewController.h
@@ -0,0 +1,13 @@
+//
+//  OTViewController.h
+//  OpenTokMOS
+//
+//  Created by Charley Robinson on 12/05/2016.
+//  Copyright (c) 2016 Charley Robinson. All rights reserved.
+//
+
+@import UIKit;
+
+@interface OTViewController : UIViewController
+
+@end
---
+++ Example/OpenTokMOS/OTViewController.m
@@ -0,0 +1,29 @@
+//
+//  OTViewController.m
+//  OpenTokMOS
+//
+//  Created by Charley Robinson on 12/05/2016.
+//  Copyright (c) 2016 Charley Robinson. All rights reserved.
+//
+
+#import "OTViewController.h"
+
+@interface OTViewController ()
+
+@end
+
+@implementation OTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
