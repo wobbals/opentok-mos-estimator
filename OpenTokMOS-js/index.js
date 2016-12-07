eb05f508de676768700696d135153c9f160a9b07
Charley Robinson <charley@tokbox.com>
2016-12-07 15:50:58 +0100

    Code cleanup

--- OpenTokMOS-js/index.js
+++ OpenTokMOS-js/index.js
@@ -1,51 +1,12 @@
-const MIN_VIDEO_BITRATE = 30000;
-
-function targetBitrateForPixelCount(pixelCount) {
-  // power function maps resolution to target bitrate, based on rumor config
-  // values, with r^2 = 0.98. We're ignoring frame rate, assume 30.
-  var y = 2.069924867 * Math.pow(Math.log10(pixelCount), 0.6250223771);
-  return Math.pow(10, y);
-}
-
-function audioScore(rtt, plr) {
-  const LOCAL_DELAY = 20; //20 msecs: typical frame duration
-  function H(x) { return (x < 0 ? 0 : 1) }
-  const a = 0 // ILBC: a=10
-  const b = 19.8
-  const c = 29.7
- 
-  //R = 94.2 − Id − Ie
-  var R = function(rtt, packetLoss) {
-      var d = rtt + LOCAL_DELAY;
-      var Id = 0.024 * d + 0.11 * (d - 177.3) * H(d - 177.3);
-
-      var P = packetLoss;
-      var Ie = a + b * Math.log(1 + c * P);
-
-      var R = 94.2 - Id - Ie;
-    
-      return R;
-  }
-
-  //For R < 0: MOS = 1
-  //For 0 R 100: MOS = 1 + 0.035 R + 7.10E-6 R(R-60)(100-R)
-  //For R > 100: MOS = 4.5
-  var MOS = function(R) {
-      if (R < 0) {
-          return 1;
-      }
-      if (R > 100) {
-          return 4.5;
-      }
-      return 1 + 0.035 * R + 7.10 / 1000000 * R * (R - 60) * (100 - R);
+function calculateVideoScore(subscriber, stats) {
+  var targetBitrateForPixelCount = function(pixelCount) {
+    // power function maps resolution to target bitrate, based on rumor config
+    // values, with r^2 = 0.98. We're ignoring frame rate, assume 30.
+    var y = 2.069924867 * Math.pow(Math.log10(pixelCount), 0.6250223771);
+    return Math.pow(10, y);
   }
   
-  return MOS(R(rtt, plr));
-}
-
-
-
-function calculateVideoScore(subscriber, stats) {
+  const MIN_VIDEO_BITRATE = 30000;
   if (stats.length < 2) {
     return 0;
   }
@@ -75,6 +36,42 @@ function calculateVideoScore(subscriber, stats) {
 }
 
 function calculateAudioScore(subscriber, stats) {
+  var audioScore = function(rtt, plr) {
+    const LOCAL_DELAY = 20; //20 msecs: typical frame duration
+    function H(x) { return (x < 0 ? 0 : 1) }
+    const a = 0 // ILBC: a=10
+    const b = 19.8
+    const c = 29.7
+ 
+    //R = 94.2 − Id − Ie
+    var R = function(rtt, packetLoss) {
+      var d = rtt + LOCAL_DELAY;
+      var Id = 0.024 * d + 0.11 * (d - 177.3) * H(d - 177.3);
+
+      var P = packetLoss;
+      var Ie = a + b * Math.log(1 + c * P);
+
+      var R = 94.2 - Id - Ie;
+    
+      return R;
+    }
+
+    //For R < 0: MOS = 1
+    //For 0 R 100: MOS = 1 + 0.035 R + 7.10E-6 R(R-60)(100-R)
+    //For R > 100: MOS = 4.5
+    var MOS = function(R) {
+      if (R < 0) {
+        return 1;
+      }
+      if (R > 100) {
+        return 4.5;
+      }
+      return 1 + 0.035 * R + 7.10 / 1000000 * R * (R - 60) * (100 - R);
+    }
+  
+    return MOS(R(rtt, plr));
+  }
+
   if (stats.length < 2) {
       return 0;
   }
